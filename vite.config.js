e1e9a9633a55b3ad2e63695e64cc7d0e09e0c667
Ernesto Alejo <ernestokarim@gmail.com>
2021-07-24 04:42:31 +0200

    Corrige la ruta de importación de la librería para utilizar Typescript.
    Change-Id: I026b4a94e446d9ffd1af040b003d242535420fbc

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 
+import * as path from 'path'
+
 import { extendLibConfig } from '@altipla/vite-config'
 
 
 export default extendLibConfig({
   build: {
+    lib: {
+      entry: path.resolve(process.cwd(), 'src', 'lib.ts'),
+    },
     rollupOptions: {
       external: ['vue', 'lodash-es'],
     },
